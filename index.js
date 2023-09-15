d9202e23950b1ccdb2580c0a5760d6afea1f7b4b
TheoNicod <theonicod@hotmail.fr>
2023-09-15 19:16:00 +0200

    V1

--- index.js
+++ index.js
@@ -21,30 +21,14 @@ app.get('/', function(req, res) {
 database.connexion();
 
 /**
- * Récupération de la position des markers au lancement du serveur
- */
-// demande à la base la pos de tous les markers
-var markers = [{ id: 1, long: 0.8787, lat: 0.7676 }, { id: 2, long: 0.9999, lat: 0.11111 }];
-
-
-/**
  * Routes
  */
 app.put('/location', function(req, res) {
     console.log("Reçu : PUT /location");
     console.log("body=" + JSON.stringify(req.body));
     res.setHeader('Content-type', 'application/json');
-
-    // res.json(req.body);
-    let location = {
-        longitude: 0.010101,
-        latitude: 0.101010,
-        image: "picture.png",
-        description: req.body.description,
-        type: req.body.type,
-    }
-    database.addLocation(location);
-
+    
+    database.addLocation(req.body);
 });
 
 app.get('/location', function(req, res) { // Location des markers seulement (sans les infos "type" "url"...)
@@ -57,16 +41,3 @@ app.get('/location', function(req, res) { // Location des markers seulement (san
     })
     
 })
-
-app.get('/location/:id', function(req, res) { // Location des markers seulement (sans les infos "type" "url"...)
-    console.log("Reçu : GET /location");
-    res.setHeader('Content-type', 'application/json');
-
-    // demande à la base les infos d'un marker selon 1 id
-    // on recup l'id avec req.params.id
-
-    res.json(markers); // Envoi de la réponse au client
-})
-
-
-
--- public/carte.js
+++ public/carte.js
@@ -1,74 +1,123 @@
 document.addEventListener("DOMContentLoaded", async function() {
 
-    function getLocation() {
-        let location = {};
-        location.type = document.getElementById("type").value;
-        location.description = document.getElementById("desc").value;
+    var myLat = undefined;
+    var myLng = undefined;
 
-        return location;
-    }
+    var atMyPosition = false;
 
-    function addLocation() {
-        console.log("hello");
-        let location = getLocation();
-
-        fetch('/location', {method: 'PUT', headers: {'Content-type': 'application/json'}, body: JSON.stringify(location)});
-    }
-
+    // Création de la map
     var map = L.map('map', {
         center: [51.505, -0.09],
         zoom: 13
     });
 
-    // // Définir les limites de la carte (au format LatLngBounds)
-    // var southWest = L.latLng(40.712, -74.227); // Coordonnée sud-ouest
-    // var northEast = L.latLng(40.774, -74.125); // Coordonnée nord-est
-    // var bounds = L.latLngBounds(southWest, northEast);
-
-    // // Appliquer les limites à la carte
-    // map.setMaxBounds(bounds);
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    
+    // Gestion de la géolocalisation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            myLat = position.coords.latitude;
+            myLng = position.coords.longitude;
+            map.setView([myLat, myLng], 12);
+        });
+    } else {
+        // La géolocalisation n'est pas prise en charge par le navigateur
+        alert("La géolocalisation n'est pas prise en charge par votre navigateur.");
+    }
 
-    map.on('click', function(e) {
-        // Récupérer les coordonnées (latitude et longitude) du clic
-        var lat = e.latlng.lat;
-        var lng = e.latlng.lng;
-
-        // Créer un marqueur et l'ajouter à la carte à l'emplacement du clic
-        var marker = L.marker([lat, lng]).addTo(map);
-    });
-
+    // Demande toutes les localisations au serveur et les affiches sur le map (TODO optimisations : ne pas charger toutes les localisations en même temps (trop lourd s'il y beaucoup de points))
     let response = await fetch("/location");
     if(response.status == 200) {
         let data = await response.json();
-        
         for(let i = 0; i < data.length; i++) {
-            L.marker([data[i].longitude, data[i].latitude])
+            L.marker([data[i].latitude, data[i].longitude])
                 .on("click", function(e) {
-                    openMarker(data[i], e);
+                    openMarker(data[i]);
                 })
                 .addTo(map);
         }
     }
 
-    function openMarker(data, e) {
-        e.stopPropagation();
-        e.preventDefault();
-        console.log("OPENED MENNN");
-        document.getElementById('opened-marker').classList.remove('hide');
-    }
+    var lat = 0;
+    var lng = 0;
+    // Récupération de la position au clique sur la map
+    map.on('click', function(e) {
+        lat = e.latlng.lat;
+        lng = e.latlng.lng;
 
-    function closed (object) {
-        object.classList.add("hide");
-        console.log("hello");
-    }
+        document.getElementById("form").classList.remove("hide"); // TODO : désactiver les evenements sur la map lorque le form est ouvert
+        atMyPosition = false;
+    });
 
-    let close = document.getElementById("close");
-    close.addEventListener("click", function(e) {
-        close.classList.add("hide");
-        console.log("coucoucocuocu");
+    // Gère le bouton "Add to my position"
+    document.getElementById("add-my-position").addEventListener("click", function() {
+        if(myLat == undefined || myLng == undefined) {
+            alert("Veuillez autoriser ce site à accéder à votre géolocation");
+        }else{
+            document.getElementById("form").classList.remove("hide");
+            atMyPosition = true;
+        }
     })
+    
+    // Gère le bouton add du formulaire
+    document.getElementById("submit-form").addEventListener("click", function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        let location = getLocation();
+
+        if(atMyPosition) {
+            location.longitude = myLng;
+            location.latitude = myLat;
+        }else{
+            location.longitude = lng;
+            location.latitude = lat;
+        }
+        
+        // Envoie les infos au serveur
+        fetch('/location', {method: 'PUT', headers: {'Content-type': 'application/json'}, body: JSON.stringify(location)});
+
+        // Ajout du markers sur la map
+        L.marker([lat, lng])
+        .on("click", function(e) { // Ajout d'une fonction au clique sur un marker
+            openMarker(location);
+        })
+        .addTo(map);
+        document.getElementById("form").classList.add("hide");
+    })
+
+    // Gère la fermeture des fenêtres
+    let close = document.getElementsByClassName("close");
+    for (let i = 0; i < close.length; i++) {
+        let cl = close[i];
+        cl.addEventListener("click", function() {
+            cl.parentNode.classList.add("hide");
+        });
+    }
+
+    /** Récupère les informations du formulaire 
+     * 
+     * @returns Object location
+     */
+    function getLocation() {
+        let location = {};
+        location.type = document.getElementById("type").value;
+        location.description = document.getElementById("desc").value;
+        location.image = "";
+
+        return location;
+    }
+
+    /** Ouvre la fenêtre d'informations sur la position cliquée et affiche les infos
+     * 
+     * @param {*} data Les infos à afficher
+     */
+    function openMarker(data) {
+        document.getElementById('opened-marker').classList.remove('hide');
+        document.getElementById("type-info").textContent = data.type;
+        // document.getElementById("image-info").textContent = data.type;
+        document.getElementById("desc-info").textContent = data.description;
+    }
 });
